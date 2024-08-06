9c19b4fe60d74d3e56d58b25c1cdc1685f9ce498
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-08-05 23:48:55 -0500

    Started work on the actual homepage

---
+++ public/js/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp  } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCg33_jbyjrGwyFHzSKp56IWq_orCt9zYA",
+    authDomain: "magnetism-simulation.firebaseapp.com",
+    projectId: "magnetism-simulation",
+    storageBucket: "magnetism-simulation.appspot.com",
+    messagingSenderId: "424173550885",
+    appId: "1:424173550885:web:94a3ef1e0297639f186ecc",
+    measurementId: "G-FQ2RVRG2GY"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
