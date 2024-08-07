f302f3e54e56296510d4ddfffae3d7ecee1b4158
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-08-07 03:31:05 -0500

    Added slightly better way of storing the api key so I can use it locally but it wont end up on github

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,14 +1,22 @@
 import { initializeApp  } from 'https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js'
 
 const firebaseConfig = {
-    apiKey: "firebase_api_key",
-    authDomain: "magnetism-simulation.firebaseapp.com",
-    projectId: "magnetism-simulation",
-    storageBucket: "magnetism-simulation.appspot.com",
-    messagingSenderId: "424173550885",
-    appId: "1:424173550885:web:94a3ef1e0297639f186ecc",
-    measurementId: "G-FQ2RVRG2GY"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: "firebase_api_key",
+  authDomain: "magnetism-simulation.firebaseapp.com",
+  projectId: "magnetism-simulation",
+  storageBucket: "magnetism-simulation.appspot.com",
+  messagingSenderId: "424173550885",
+  appId: "1:424173550885:web:94a3ef1e0297639f186ecc",
+  measurementId: "G-FQ2RVRG2GY"
+};
+
+if (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+{
+  const response = await fetch("/api_key.txt")
+  const text = await response.text()
+  firebaseConfig.apiKey = text
+}
+
+const app = initializeApp(firebaseConfig);
+
+export { app }
